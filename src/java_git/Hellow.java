fb11e1dddc19e96cc8d4db6442901144417415be
DESKTOP-0OVEPL3\USER <yunjh0618@gmail.com>
2023-06-19 15:00:45 +0900

    commit 3

--- src/java_git/Hellow.java
+++ src/java_git/Hellow.java
@@ -3,7 +3,7 @@ package java_git;
 public class Hellow {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("아행행!! 하오");
 		System.out.println("아행행!! 하오");
 	}
 
